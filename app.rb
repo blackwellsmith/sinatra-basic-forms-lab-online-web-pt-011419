b8d6b023acfc4b3c027646bf687292011f85855c
blackwell <blackwellsmithiv@gmail.com>
2019-04-17 22:55:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   post '/puppy' do
     @name = params[:name]
     @breed = params[:breed]
-    @age = params[:months]
+    @age = params[:months_old]
 
     erb :display_puppy
   end
